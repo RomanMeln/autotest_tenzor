251c2517c247a96b09e869684836639e0b5bfa29
RomanMeln <tupozarabotok@gmail.com>
2024-04-17 19:30:30 +0300

    Переписан код для исключения time.sleep(). Добавлен README.md

--- autotest_testtask_1.py
+++ autotest_testtask_1.py
@@ -12,26 +12,37 @@ def test_tests_task_1():
     driver = webdriver.Chrome()
     driver.get(url=url)
 
-    time.sleep(5)
     # поиск и переход на страницу контакты
     WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.CLASS_NAME, "sbisru-Header"))
     ).find_elements(By.TAG_NAME, 'a')[1].click()
 
     # поиск и активация элемента тензор
-    driver.find_element(By.ID, 'contacts_clients').find_element(By.CSS_SELECTOR, '[title="tensor.ru"]').click()
+    # driver.find_element(By.ID, 'contacts_clients').find_element(By.CSS_SELECTOR, '[title="tensor.ru"]').click()
+    WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, '//*[@id="contacts_clients"]/div[1]/div/div/div[2]/div/a'))
+    ).click()
 
     # переход на страницу тензор
     driver.switch_to.window(driver.window_handles[1])
     time.sleep(4)
 
     # поиск и проверка наличия элемента
-    power_in_human = driver.find_element(By.CLASS_NAME, 'tensor_ru-Index__block4-bg')
+    # power_in_human = driver.find_element(By.CLASS_NAME, 'tensor_ru-Index__block4-bg')
+    power_in_human = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CLASS_NAME, 'tensor_ru-Index__block4-bg'))
+    )
+    # присваивание текста элемента в переменную
     text_power_in_human = power_in_human.text
-    time.sleep(2)
+    
     if 'Сила в людях' in text_power_in_human:
         power_in_human.find_element(By.XPATH, '//a[@href="/about"]').click()
-        img = driver.find_elements(By.CLASS_NAME, 'tensor_ru-About__block3-image')
+        # time.sleep(5)
+
+        # img = driver.find_elements(By.CLASS_NAME, 'tensor_ru-About__block3-image')
+        img = WebDriverWait(driver, 10).until(
+            EC.presence_of_all_elements_located((By.CLASS_NAME, 'tensor_ru-About__block3-image'))
+        )
 
         width_img = 0
         height_img = 0
@@ -56,13 +67,8 @@ def test_tests_task_1():
 
             assert width_img == width
             assert height_img == height
-        print(f'Ширина: {width_img}, Высота: {height_img}')
-    else:
-        print('Элемент со сзначением \"Сила в людях\" отсутствует')
 
     assert 'Сила в людях' in text_power_in_human
-
-    time.sleep(3)
 
     driver.close()
     driver.quit()
--- autotest_testtask_2.py
+++ autotest_testtask_2.py
@@ -5,9 +5,19 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import pytest
 
+
 def get_region(use_driver):
-    region = use_driver.find_elements(By.CLASS_NAME, 'sbis_ru-Region-Chooser')[1].text
+    # region = use_driver.find_elements(By.CLASS_NAME, 'sbis_ru-Region-Chooser')[1].text
+    region = WebDriverWait(use_driver, 10).until(
+        EC.presence_of_all_elements_located((By.CLASS_NAME, 'sbis_ru-Region-Chooser'))
+    )[1].text
     return region
+
+
+def get_partners_list(use_driver):
+    return WebDriverWait(use_driver, 10).until(
+        EC.presence_of_all_elements_located((By.CLASS_NAME, 'sbisru-Contacts-List__col-1'))
+    )[0].text
 
 
 url = "https://sbis.ru/"
@@ -24,23 +34,30 @@ def test_tests_task_2():
 
     # поиск и определение региона и листа партнеров
     region = get_region(driver)
-    partners_list = driver.find_elements(By.CLASS_NAME, 'sbisru-Contacts-List__col-1')[0].text
+    partners_list = get_partners_list(driver)
 
     if len(region) > 0:
         if len(partners_list) > 0:
             # активировать элемент название региона
             driver.find_element(By.CLASS_NAME, 'sbis_ru-Region-Chooser__text').click()
-            time.sleep(5)
+            # WebDriverWait(driver, 10).until(
+            #     EC.presence_of_element_located((By.CLASS_NAME, 'sbis_ru-Region-Chooser__text'))
+            # ).click()
+            time.sleep(2)
 
             # выбор камчатского края в открывшемся окне
             driver.find_element(By.CSS_SELECTOR, '[title="Камчатский край"]').click()
-            time.sleep(5)
+            # WebDriverWait(driver, 10).until(
+            #     EC.presence_of_element_located((By.CSS_SELECTOR, '[title="Камчатский край"]'))
+            # ).click()
+            time.sleep(2)
+
 
             # определение региона, листа партнеров, текущего url, title
             new_region = get_region(driver)
-            new_partner_list = driver.find_elements(By.CLASS_NAME, 'sbisru-Contacts-List__col-1')[0].text
+            new_partner_list = get_partners_list(driver)
             new_url = driver.current_url
-            title = driver.find_element(By.TAG_NAME, 'title').get_attribute('innerHTML')
+            title = driver.title
 
             assert len(region) > 0
             assert len(partners_list) > 0
@@ -49,7 +66,6 @@ def test_tests_task_2():
             assert new_partner_list != partners_list
             assert 'Камчатский' in title
 
-    time.sleep(1)
     driver.close()
     driver.quit()
 
